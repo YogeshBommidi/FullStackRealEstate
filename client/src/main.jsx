4e7bcede34391270c91d615d3601550fc1cbe60d
Yogesh <yogeshbommidi@gmail.com>
2024-05-04 12:31:28 +0530

    final commit

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,13 +7,13 @@ import { Auth0Provider } from "@auth0/auth0-react";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Auth0Provider
-    domain="dev-y7bvjgidpuz83cli.us.auth0.com"
-    clientId="ds4BzSTsdT8HTdzMttzrVf6OscGnj7KA"
-    authorizationParams={{
-      redirect_uri:"https://full-stack-real-estate-steel.vercel.app"
-    }}
-    audience = "http://localhost:8000"
-    scope = "openid profile email"
+      domain="dev-y7bvjgidpuz83cli.us.auth0.com"
+      clientId="ds4BzSTsdT8HTdzMttzrVf6OscGnj7KA"
+      authorizationParams={{
+        redirect_uri: "https://full-stack-real-estate-steel.vercel.app",
+      }}
+      audience="http://localhost:8000"
+      scope="openid profile email"
     >
       <App />
     </Auth0Provider>
